41fa8517848c91fb8b141552b64390730e83a2f9
KM <tsnok777@gmail.com>
2022-07-12 15:44:22 +0300

    page with parametrs

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('index.html')
 
-@app.route('/first')
-def first_view():
-    return 'This is first page'
+@app.route('/about')
+def about_view():
+    return flask.render_template('about.html')
 
-@app.route('/second')
-def second_view():
-    return 'This is second page'
+@app.route('/welcome')
+def welcome_view():
+    welcome = 'WELCOM'
+    number = '777'
+    num_weeks = '8'
+    return flask.render_template('welcome.html',
+                                 welcome=welcome,
+                                 number=number,
+                                 num_weeks=num_weeks,)
 
 @app.route('/third')
 def third_view():
